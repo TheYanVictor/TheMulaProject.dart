be45e2758d494746460efea481166527390bcb86
ropasdef <111322622+ropasdef@users.noreply.github.com>
2023-10-14 00:50:14 -0300

    tmalss

--- mulaProject/lib/main.dart
+++ mulaProject/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:trabalho_01/view/financeiro_main_view.dart';
 import 'package:trabalho_01/view/login_view.dart';
 import 'package:trabalho_01/view/menu_view.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -22,7 +23,7 @@ void main() {
             'main_menu': (context) => menu_view(),
 
             //Following pages
-            //'financeiro' : (context) => financeiro_main_view(),
+            'financeiro': (context) => financeiro_main_view(),
             //'listas' : (context) => listas_main_view(),
             //'perfil' : (context) => perfil_main_view(),
             //'grupos' : (context) => grupos_main_view(),
--- mulaProject/lib/view/menu_view.dart
+++ mulaProject/lib/view/menu_view.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/src/widgets/navigator.dart';
 
 // Creating the main menu view
 
@@ -12,26 +16,56 @@ class menu_view extends StatefulWidget {
 class _menu_view extends State<menu_view> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(100),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             //row 1 -
+            //Row(                                                      //errado?
+            //add code
+            //children: [
+            SizedBox(
+              height: 10,
+            ),
             Row(
-              //add code
+              mainAxisAlignment: MainAxisAlignment.start,
+              //crossAxisAlignment:CrossAxisAlignment.values(20, 50),
               children: [
-                SizedBox(width: double.infinity),
-                Icon(Icons.account_balance_sharp,
-                    size: 100, color: Color.fromRGBO(198, 37, 198, 0.612)),
-                TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Saldo'),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.account_balance,
+                          size: 70,
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, 'financeiro');
+                        },
+                      ),
+                    ),
+                    Container(
+                      height: 60,
+                      width: 80,
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                        'Saldo',
+                        style: TextStyle(fontSize: 25),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
+            //],                                                     errado?
+            //),                                                      errado?
             //row 2
             //Row(
             //add code
