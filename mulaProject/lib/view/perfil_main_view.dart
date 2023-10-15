278e31695f02c45af10dbb2adc05da912c709d59
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-10-15 19:26:31 -0300

    Add foto, nome e legenda na pagina de perfil

--- mulaProject/lib/view/perfil_main_view.dart
+++ mulaProject/lib/view/perfil_main_view.dart
@@ -11,16 +11,101 @@ class _perfil_main_viewState extends State<perfil_main_view> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //
+      //App Bar provisória
+      //
+      appBar: AppBar(
+        backgroundColor: Color(0xFFF24C3D),
+        toolbarHeight: 200,
+
+        //
+        // Texto e botoes do app bar
+        //
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //
+            //Voltar para a pagina anterior                      falta alinhar para cima
+            //
+            Container(
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                iconSize: 40,
+                color: Colors.white,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            //
+            //Texto central
+            //
+            Text(
+              'Perfil',
+              style: TextStyle(color: Colors.white),
+            ),
+            //
+            //Botao de home                                     falta alinhar para cima
+            //
+            Container(
+              child: IconButton(
+                icon: const Icon(Icons.house),
+                iconSize: 40,
+                color: Colors.white,
+                onPressed: () {
+                  Navigator.pushNamed(context, 'main_menu');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Padding(
         padding: EdgeInsets.all(10),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(width: double.infinity, height: 0),
-            Text(
-              'Perfil',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            //
+            //Stack para sobrepor a imagem do icone
+            //
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                //
+                //Container de alinhamento da imagem
+                //
+                Container(
+                  alignment: Alignment.topCenter,
+                  child:
+                      //
+                      //Corta a imagem para deixar redonda
+                      Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.network(
+                          'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/78473e74-db74-4dea-99e2-126f30e136bf/deh9wem-ccce3dcb-0348-42ce-9f1d-6b313fec1304.png/v1/fit/w_828,h_622,q_70,strp/whitebeard_by_newgate_arts_deh9wem-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9Mjg4MCIsInBhdGgiOiJcL2ZcLzc4NDczZTc0LWRiNzQtNGRlYS05OWUyLTEyNmYzMGUxMzZiZlwvZGVoOXdlbS1jY2NlM2RjYi0wMzQ4LTQyY2UtOWYxZC02YjMxM2ZlYzEzMDQucG5nIiwid2lkdGgiOiI8PTM4NDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.JY1RWVsHL0a2wMt5KWDwEe0DIuwZj3OJq5q-R6YAFQk',
+                          fit: BoxFit.cover,
+                          height: 150,
+                          width: 150,
+                        ),
+                      ),
+                      //
+                      //Nome e legenda
+                      //
+                      Text(
+                        'Nome da pessoa',
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      Text(
+                        'Frase motivacional que ninguem vai ler',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
