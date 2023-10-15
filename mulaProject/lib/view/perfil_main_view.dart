d97c6419a940d55783382b40399d26999a801c6c
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-10-15 19:34:43 -0300

    Ajustes no tamanho da imagem

--- mulaProject/lib/view/perfil_main_view.dart
+++ mulaProject/lib/view/perfil_main_view.dart
@@ -16,7 +16,7 @@ class _perfil_main_viewState extends State<perfil_main_view> {
       //
       appBar: AppBar(
         backgroundColor: Color(0xFFF24C3D),
-        toolbarHeight: 200,
+        toolbarHeight: 150,
 
         //
         // Texto e botoes do app bar
@@ -79,7 +79,7 @@ class _perfil_main_viewState extends State<perfil_main_view> {
                   alignment: Alignment.topCenter,
                   child:
                       //
-                      //Corta a imagem para deixar redonda
+                      //IMAGEM
                       Column(
                     children: [
                       ClipRRect(
@@ -87,8 +87,11 @@ class _perfil_main_viewState extends State<perfil_main_view> {
                         child: Image.network(
                           'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/78473e74-db74-4dea-99e2-126f30e136bf/deh9wem-ccce3dcb-0348-42ce-9f1d-6b313fec1304.png/v1/fit/w_828,h_622,q_70,strp/whitebeard_by_newgate_arts_deh9wem-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9Mjg4MCIsInBhdGgiOiJcL2ZcLzc4NDczZTc0LWRiNzQtNGRlYS05OWUyLTEyNmYzMGUxMzZiZlwvZGVoOXdlbS1jY2NlM2RjYi0wMzQ4LTQyY2UtOWYxZC02YjMxM2ZlYzEzMDQucG5nIiwid2lkdGgiOiI8PTM4NDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.JY1RWVsHL0a2wMt5KWDwEe0DIuwZj3OJq5q-R6YAFQk',
                           fit: BoxFit.cover,
-                          height: 150,
-                          width: 150,
+                          //
+                          //Tamanho da imagem
+                          //
+                          height: 170,
+                          width: 170,
                         ),
                       ),
                       //
