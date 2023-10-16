858f12d728f64893ad9b55e4c1c6e9eb04d9c8e7
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-10-15 21:47:40 -0300

    Criação da barra de procura na pagina produtos

--- mulaProject/lib/view/produtos_main_view.dart
+++ mulaProject/lib/view/produtos_main_view.dart
@@ -11,19 +11,91 @@ class _produtos_main_viewState extends State<produtos_main_view> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
+      //
+      //App Bar provisória
+      //
+      appBar: AppBar(
+        backgroundColor: Color(0xFFF24C3D),
+        toolbarHeight: 150,
+
+        //
+        // Texto e botoes do app bar
+        //
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(width: double.infinity, height: 0),
+            //
+            //Voltar para a pagina anterior                      falta alinhar para cima
+            //
+            Container(
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                iconSize: 40,
+                color: Colors.white,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            //
+            //Texto central
+            //
             Text(
               'Produtos',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            //
+            //Botao de home                                     falta alinhar para cima
+            //
+            Container(
+              child: IconButton(
+                icon: const Icon(Icons.house),
+                iconSize: 40,
+                color: Colors.white,
+                onPressed: () {
+                  Navigator.pushNamed(context, 'main_menu');
+                },
+              ),
             ),
           ],
         ),
+      ),
+
+      body:
+          //      Campo de procura
+          Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            //espaço do appbar
+            height: 150,
+          ),
+          Container(
+            //Caixa do texto
+            alignment: Alignment.centerLeft,
+            //tamanho do quadrado
+            width: 300,
+            height: 90,
+            child: TextField(
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black38, width: 5),
+                    borderRadius: BorderRadius.horizontal(
+                      left: Radius.circular(40),
+                      right: Radius.circular(40),
+                    ),
+                  ),
+                  label: Text(
+                    '  Procurar',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                        fontSize: 27),
+                  )),
+            ),
+          ),
+        ],
       ),
     );
   }
