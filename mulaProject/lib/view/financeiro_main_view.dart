5812e3aa6e4d75ec94e0e5eab9e1833973f0b03e
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-10-18 23:36:22 -0300

    Cartao da pag financeiro

--- mulaProject/lib/view/financeiro_main_view.dart
+++ mulaProject/lib/view/financeiro_main_view.dart
@@ -13,19 +13,96 @@ class _financeiro_main_view extends State<financeiro_main_view> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
+      //
+      //App Bar provisória
+      //
+      appBar: AppBar(
+        backgroundColor: Color(0xFFF24C3D),
+        toolbarHeight: 150,
+
+        //
+        // Texto e botoes do app bar
+        //
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(width: double.infinity, height: 0),
+            //
+            //Voltar para a pagina anterior                      falta alinhar para cima
+            //
+            Container(
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                iconSize: 40,
+                color: Colors.white,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            //
+            //Texto central
+            //
             Text(
-              'Saldo',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              'Financeiro',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            //
+            //Botao de home                                     falta alinhar para cima
+            //
+            Container(
+              child: IconButton(
+                icon: const Icon(Icons.house),
+                iconSize: 40,
+                color: Colors.white,
+                onPressed: () {
+                  Navigator.pushNamed(context, 'main_menu');
+                },
+              ),
             ),
           ],
         ),
+      ),
+//------------------------------------------------------------------------------
+
+      body: Column(
+        //alinhamento da coluna principal
+        mainAxisAlignment: MainAxisAlignment.start,
+
+        children: [
+          // Area dos cartoes
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                //color: Colors.black,
+                width: 200,
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    SizedBox(
+                      height: 150,
+                      width: 200,
+                      child: Card(
+                        semanticContainer: true,
+                        color: Colors.amberAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        child: Text(
+                          'Card',
+                          style: TextStyle(fontSize: 40),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
