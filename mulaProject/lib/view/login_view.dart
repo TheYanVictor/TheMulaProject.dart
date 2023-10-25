349b954117729c97bc98eab7db57344fb6bd18f3
ropasdef <luc.augusto12@gmail.com>
2023-10-25 12:37:40 -0300

    CONSEGUI

--- mulaProject/lib/view/login_view.dart
+++ mulaProject/lib/view/login_view.dart
@@ -17,7 +17,7 @@ class login_view extends StatefulWidget {
 class _login_viewState extends State<login_view> {
   //estado da senha
   bool senhaInvisivel = true;
-  String email = "";
+  var email = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -38,14 +38,15 @@ class _login_viewState extends State<login_view> {
           ),
           TextField(
             onSubmitted: (value) {
-              if(value == ''){
-                //avisar q deu merda
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('O e-mail deve ser válido')));
-              }else{
+              if (value == '') {
+                //avisar q deu errado
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('O e-mail deve ser válido')));
+              } else {
                 //ser feliz
               }
             },
-            controller: TextEditingController(text: email),
+            controller: email,
             decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.horizontal(
@@ -66,10 +67,11 @@ class _login_viewState extends State<login_view> {
           ),
           TextField(
             onSubmitted: (value) {
-              if(value == ''){
+              if (value == '') {
                 //avisar q deu merda
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('A senha deve ser válida')));
-              }else{
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('A senha deve ser válida')));
+              } else {
                 //ser feliz
               }
             },
@@ -92,14 +94,13 @@ class _login_viewState extends State<login_view> {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                     onPressed: () {
-                          bool estado = senhaInvisivel;
-                          //funcionalidade de "desobscurecer" o texto do textField
-                          setState(() {
-                            senhaInvisivel = !estado;
-                          });
-                          
-                        },
+                    onPressed: () {
+                      bool estado = senhaInvisivel;
+                      //funcionalidade de "desobscurecer" o texto do textField
+                      setState(() {
+                        senhaInvisivel = !estado;
+                      });
+                    },
                     style: TextButton.styleFrom(
                       minimumSize: Size.zero,
                       padding: EdgeInsets.zero,
@@ -123,10 +124,22 @@ class _login_viewState extends State<login_view> {
           ),
           OutlinedButton(
             onPressed: () {
-              Navigator.pushNamed(
-                context,
-                'main_menu',
-              );
+              var txtEmail;
+              setState(() {
+                txtEmail = email.text;
+              });
+
+              if (txtEmail == '') {
+                //avisar q deu errado
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('O e-mail deve ser válido')));
+              } else {
+                //ser feliz
+                Navigator.pushNamed(
+                  context,
+                  'main_menu',
+                );
+              }
             },
             child: Text(
               'Entrar',
@@ -181,63 +194,59 @@ class _login_viewState extends State<login_view> {
     ));
   }
 
-
   //Caixa popUp que aparece caso o usuário tenha esquecido a senha
   Future openDialog() => showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+            //define o formato da caixa com as bordas mais arredondadas
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            //titulo da caixa
+            title: Text(
+              'Esqueceu a senha?',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
 
-    context: context,
+            //conteudo da caixa - TextField
+            content: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.horizontal(
+                      left: Radius.circular(10),
+                      right: Radius.circular(10),
+                    ),
+                  ),
+                  label: Text(
+                    'Digite seu E-mail de login',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                        fontSize: 16),
+                  )),
+            ),
 
-    builder: (context) => AlertDialog(
-
-      //define o formato da caixa com as bordas mais arredondadas
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
-      //titulo da caixa
-      title: Text('Esqueceu a senha?', style: TextStyle(fontWeight: FontWeight.bold),),
-
-      //conteudo da caixa - TextField
-      content: TextField(
-            decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.horizontal(
-                    left: Radius.circular(10),
-                    right: Radius.circular(10),
+            //botão de confirmação
+            actions: [
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  'Recuperar Senha',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.getFont('Inter'),
+                ),
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Color(0xFFF24C3D),
+                  foregroundColor: Color(0xFFF6F6F6),
+                  minimumSize: Size(double.infinity, 50),
+                  textStyle:
+                      TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
                   ),
                 ),
-                label: Text(
-                  'Digite seu E-mail de login',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                      fontSize: 16),
-                )),
-          ),
-
-      //botão de confirmação
-      actions: [
-        OutlinedButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text(
-              'Recuperar Senha',
-              textAlign: TextAlign.center,
-              style: GoogleFonts.getFont('Inter'),
-            ),
-            style: OutlinedButton.styleFrom(
-              backgroundColor: Color(0xFFF24C3D),
-              foregroundColor: Color(0xFFF6F6F6),
-              minimumSize: Size(double.infinity, 50),
-              textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-              ),
-            ),
-        )
-      ],
-    )
-  );
-
-  
-
+              )
+            ],
+          ));
 }
-
